9a09f2d55478d26a40fe2ce734cdc20fe0801d6d
Pavel Penev <pavpen@gmail.com>
2016-01-14 11:51:56 -0500

    Added unit tests for deserializing `Person` objects.

---
+++ NationBuilderAPIv1UnitTests/V1/Serialization/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Utilities;
+using NationBuilderAPI.V1;
+using NationBuilderAPI.V1.Http;
+
+namespace NationBuilderAPIv1UnitTests.V1.Serialization
+{
+    [TestClass]
+    public class Person
+    {
+        [TestMethod]
+        public void ShowPersonDeserialization()
+        {
+            var personJson = ShowPersonResponse1_json.Value;
+            var httpTransport = new NationBuilderHttpTransport();
+
+            PersonResponse res = httpTransport.DeserializeNationBuilderObject<PersonResponse>(personJson.ToMemoryStream());
+        }
+    }
+}
---
+++ Utilities/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Utilities
+{
+    public static class StringExtensions
+    {
+        public static MemoryStream ToMemoryStream(this string self)
+        {
+            var res = new MemoryStream(self.Length);
+            var writer = new StreamWriter(res);
+
+            writer.Write(self);
+            writer.Flush();
+            res.Position = 0;
+
+            return res;
+        }
+    }
+}
